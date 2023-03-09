1a437967ffdeee8a55c570c6c9ab6f20d04b5ca6
github_username <samanthakaras1@gmail.com>
2023-03-09 09:02:28 -0500

    update

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+console.log($)
+        $(document).ready(function(){
+          $("#reading-button").click(function(){
+            $(".test").fadeOut();
+            $(".test").fadeOut("fast");
+          });
+        });
+
+        $(document).ready(function() {
+        $("#first-event").hide();
+        $("#second-event").hide();
+        $("#third-event").hide();
+        $("#fourth-event").hide();
+        $("#fifth-event").hide();
+        $("#sixth-event").hide();
+        $("#seventh-event").hide();
+        $("#eighth-event").hide();
+        $("#ninth-event").hide();
+        $("#tenth-event").hide();
+        $("#reading-button").click(function() {
+            $("#first-event").show();
+        });
+        });
+
+        $("#reading-button").click(function() {
+        $("#first-event").hide();
+        $("#second-event").show();
+         });
+
+
 console.log("linked")
   // visual html elements
 // const columnOne = document.querySelector(".one")
@@ -8,12 +38,14 @@ console.log("linked")
 // c1c1.classList.add("theHighPriestess")
 
 // Starter screen for user to enter name for beginning of game
-const firstButton = document.getElementById("first-button");
+const startButton = document.getElementById("start-button");
 const introScreen = document.getElementById("intro-screen");
 const gameScreen = document.getElementById("game-screen");
 const playerNameInput = document.getElementById("player-name-input");
 const playerNameDisplay = document.getElementById("player-name-display");
-
+const lifeEvent = document.querySelector(".lifeEvent");
+const messageVisual = document.querySelector(".message");
+const readingResult = document.querySelector(".readingResult")
 
 
 // function that dictates player name for the game
@@ -22,21 +54,48 @@ function startGame() {
   playerNameDisplay.textContent = `Hello, ${playerName}.`;
   introScreen.style.display = "none";
   gameScreen.style.display = "block";
+  lifeEvent.innerText = lifeEvents[state.currentLifeEvent];
 }
+
+console.log(currentLifeEvent)
 
 // BELOW CODE: code for button to start shuffled deck reading displayed on browser
 
+startButton.addEventListener("click", startGame);
+const readingButton = document.getElementById('reading-button');
 
-firstButton.addEventListener("click", startGame);
-const startButton = document.getElementById('start-button');
+// BELOW CODE: used to keep track of the number of "bad" and "good" cards that appear during a state.reading.
 
-// BELOW CODE: used to keep track of the number of "bad" and "good" cards that appear during a reading.
-const reading = {
-  badCards: 0,
-  goodCards: 0,
+const lifeEvents = [
+  "College is the first step into our journey. Will you graduate on good or bad terms?",
+  "Next, you are about to go on a date with your dream person. Is love on the horizon?",
+  "You just landed an interview for your first adult job! Will it go well?",
+  "Now let's check on your career trajectory - are you climbing the ladder or stuck trying to move up?",
+  "I hear wedding bells! Does it look like a good future for the two of you?",
+  "It's about that time for your very first home purchase. Did you make the right decision?",
+  "First baby is on the way! Does the future look bright and healthy for your family?",
+  "Oh no! It's your first major health scare. Do the cards suspect something terrible?",
+  "Looks like you're experiencing a mid-life crisis (don't worry we all have one). How's it going for you?",
+  "Lastly, it's time for retirement and beyond. How is it looking for you?",
+]
+
+
+const state = {
+  reading: {
+    badCards: 0,
+    goodCards: 0,
+  },
+  lifePoints: 0,
+  currentLifeEvent: 0
 }
 
-startButton.addEventListener('click', () => {
+function nextLifeEvent(){
+  // hide the reading result div by toggling hidden class
+  // set state.currentLifeEvent to += 1
+}
+
+
+readingButton.addEventListener('click', () => {
 
 // variable for the tarot cards array in the deck
 const tarotCards = [
@@ -161,23 +220,28 @@ cards.forEach((card, index) => {
   console.log(num);
   if (num === 0) {
     card.classList.add("flipped");
-    reading.badCards += 1;
+    state.reading.badCards += 1;
     // Bad card, so it counts as a negative point in the reading
-    if (reading.badCards >= badCardsMin) {
-      message = "You got 6 or more bad cards. You lose this round!";
-      console.log(message);
-    }
-  } else {
-    // Good card, so it counts as a positive point in the reading
-    reading.goodCards += 1;
-    if (reading.goodCards >= goodCardsMin) {
-      message = "You got 6 or more good cards. You win this round!";
-      console.log(message);
-    }
+    console.log("You got a bad card.")
   }
+  else {
+    state.reading.goodCards += 1;
 
+  }
 });
+//finished looping through cards and counting which are good or bad
 
+if (state.reading.badCards >= badCardsMin) {
+  message = "You got 6 or more bad cards. You lose this round!";
+  console.log(message);
+}
+else {
+  message = "You got 6 or more good cards. You win this round!";
+  console.log(message);
+}
+
+messageVisual.innerText = message;
+readingResult.classList.toggle("hidden");
 
 // return a number between a min and a max
 function getRandomNum (min, max) {
